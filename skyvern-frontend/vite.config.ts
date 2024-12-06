66ce5cc8a2d57f7b717427eaff481575e0822041
nabilsuper <nabil@hiresuper.com>
2024-12-06 13:57:58 +0000

    Config

--- skyvern-frontend/vite.config.ts
+++ skyvern-frontend/vite.config.ts
@@ -5,6 +5,10 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: false,
+    sourcemap: false,
+  },
   server: {
     port: 8080,
   },
